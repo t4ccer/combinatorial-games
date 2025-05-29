90d803a4653209cb84f234df3f12944d76efbcda
t4ccer <t4ccer@gmail.com>
2025-05-29 16:49:39 +0200

    Prove Proposition 6.1

--- CombinatorialGames/Misere.lean
+++ CombinatorialGames/Misere.lean
@@ -52,6 +52,50 @@ def MisereEq (g h : IGame) : Prop := ∀ (x : IGame), MisereOutcome (g + x) = Mi
 
 def MisereGe (g h : IGame) : Prop := ∀ (x : IGame), MisereOutcome (g + x) ≥ MisereOutcome (h + x)
 
+theorem Outcome.ge_R (o : Outcome) : o ≥ Outcome.R := by
+  simp only [ge_iff_le]
+  unfold LE.le
+  simp [instLEOutcome]
+  cases o
+  all_goals simp [Outcome.le', Outcome.lt']
+
+theorem Outcome.L_ge (o : Outcome) : Outcome.L ≥ o := by
+  simp only [ge_iff_le]
+  unfold LE.le
+  simp [instLEOutcome]
+  cases o
+  all_goals simp [Outcome.le', Outcome.lt']
+
+theorem Outcome.ge_PN {o : Outcome} (hp : o ≥ Outcome.P) (hn : o ≥ Outcome.N) : o = Outcome.L := by
+  cases o
+  all_goals simp [Outcome.le', Outcome.lt', hp, hn, LE.le] at *
+
+theorem proposition6_1 (g h : IGame) :
+  MisereGe g h ↔ (∀ (x : IGame),
+  (MisereOutcome (h + x) ≥ Outcome.P → MisereOutcome (g + x) ≥ Outcome.P) ∧
+  (MisereOutcome (h + x) ≥ Outcome.N → MisereOutcome (g + x) ≥ Outcome.N)) := by
+  constructor <;> intro h1
+  · intro x
+    unfold MisereGe at h1
+    constructor <;> intro h2
+    · exact Preorder.le_trans Outcome.P (MisereOutcome (h + x)) (MisereOutcome (g + x)) h2 (h1 x)
+    · exact Preorder.le_trans Outcome.N (MisereOutcome (h + x)) (MisereOutcome (g + x)) h2 (h1 x)
+  · unfold MisereGe
+    intro x
+    obtain ⟨h2, h3⟩ := h1 x
+    cases ho : MisereOutcome (h + x)
+    · rw [ho] at h2
+      rw [ho] at h3
+      have h4 := h2 (Outcome.L_ge Outcome.P)
+      have h5 := h3 (Outcome.L_ge Outcome.N)
+      have h6 := Outcome.ge_PN h4 h5
+      exact le_of_eq (Eq.symm h6)
+    · rw [ho] at h3
+      exact h3 (Preorder.le_refl Outcome.N)
+    · rw [ho] at h2
+      exact h2 (Preorder.le_refl Outcome.P)
+    · exact Outcome.ge_R (MisereOutcome (g + x))
+
 noncomputable def Adjoint (g : IGame) : IGame :=
   have _ := Classical.propDecidable (g = 0)
   have _ := Classical.propDecidable (g.leftMoves = ∅)
