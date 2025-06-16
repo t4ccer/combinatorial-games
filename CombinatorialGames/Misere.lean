60213e9592b0667789316ae06b97f0316c5e7aa7
t4ccer <t4ccer@gmail.com>
2025-06-16 16:05:39 +0200

    More 6.4

--- CombinatorialGames/Misere.lean
+++ CombinatorialGames/Misere.lean
@@ -2,10 +2,47 @@ import CombinatorialGames.Game.IGame
 import CombinatorialGames.Game.Special
 import Mathlib.Data.Set.Operations
 
-inductive Outcome where
+inductive PlayerOutcome where
+  /-- Left wins going first -/
   | L
+  /-- Right wins going first -/
+  | R
+
+mutual
+
+def MisereOutcomeL_isL (g : IGame): Prop :=
+  IGame.leftMoves g = ∅ ∨ (∃ gl ∈ g.leftMoves, ¬MisereOutcomeR_isR gl)
+termination_by g
+decreasing_by
+  -- TODO: Do something so we can see here that gl ∈ g.leftMoves
+  sorry
+
+def MisereOutcomeR_isR (g : IGame) : Prop :=
+  IGame.rightMoves g = ∅ ∨ (∃ gr ∈ g.rightMoves, ¬MisereOutcomeL_isL gr)
+termination_by g
+decreasing_by
+  sorry
+
+end
+
+/-- Game outcome if Left goes first -/
+noncomputable def MisereOutcomeL (g : IGame) : PlayerOutcome :=
+  have _ := Classical.propDecidable (MisereOutcomeL_isL g)
+  if MisereOutcomeL_isL g then PlayerOutcome.L else PlayerOutcome.R
+
+/-- Game outcome if Right goes first -/
+noncomputable def MisereOutcomeR (g : IGame) : PlayerOutcome :=
+  have _ := Classical.propDecidable (MisereOutcomeR_isR g)
+  if MisereOutcomeR_isR g then PlayerOutcome.R else PlayerOutcome.L
+
+inductive Outcome where
+  /-- Left wins -/
+  | L
+  /-- Next player wins -/
   | N
+  /-- Previous (second) player wins -/
   | P
+  /-- Right wins -/
   | R
 
 def Outcome.lt' (lhs rhs : Outcome) : Prop :=
@@ -45,8 +82,14 @@ theorem Outcome.le_antisymm' (a b : Outcome) (hab : Outcome.le' a b) (hba : Outc
 instance : PartialOrder Outcome where
   le_antisymm := Outcome.le_antisymm'
 
--- TODO
-def MisereOutcome : IGame → Outcome := sorry
+def PlayerOutcomesToGameOutcome : PlayerOutcome → PlayerOutcome → Outcome
+  | PlayerOutcome.L, PlayerOutcome.L => Outcome.L
+  | PlayerOutcome.R, PlayerOutcome.R => Outcome.R
+  | PlayerOutcome.R, PlayerOutcome.L => Outcome.P
+  | PlayerOutcome.L, PlayerOutcome.R => Outcome.N
+
+noncomputable def MisereOutcome : IGame → Outcome :=
+  fun g => PlayerOutcomesToGameOutcome (MisereOutcomeL g) (MisereOutcomeR g)
 
 def MisereEq (g h : IGame) : Prop := ∀ (x : IGame), MisereOutcome (g + x) = MisereOutcome (h + x)
 
@@ -73,26 +116,36 @@ theorem proposition6_1 (g h : IGame) :
   (MisereOutcome (h + x) ≥ Outcome.P → MisereOutcome (g + x) ≥ Outcome.P) ∧
   (MisereOutcome (h + x) ≥ Outcome.N → MisereOutcome (g + x) ≥ Outcome.N)) := by
   constructor <;> intro h1
-  · intro x
+  · -- => is immediate
+    intro x
     unfold MisereGe at h1
     constructor <;> intro h2
     · exact Preorder.le_trans Outcome.P (MisereOutcome (h + x)) (MisereOutcome (g + x)) h2 (h1 x)
     · exact Preorder.le_trans Outcome.N (MisereOutcome (h + x)) (MisereOutcome (g + x)) h2 (h1 x)
-  · unfold MisereGe
+  · -- For the converse, we must show that o(G + X) > o(H + X), for all X.
+    unfold MisereGe
     intro x
     obtain ⟨h2, h3⟩ := h1 x
-    cases ho : MisereOutcome (h + x)
-    · rw [ho] at h2
+    cases ho : MisereOutcome (h + x) with
+    | R =>
+      -- If o(H + X) = R, then there is nothing to prove
+      exact Outcome.ge_R (MisereOutcome (g + x))
+    | N =>
+      -- If o(H + X) = P or N, it is immediate from (i) or (ii)
+      rw [ho] at h3
+      exact h3 (Preorder.le_refl Outcome.N)
+    | P =>
+      rw [ho] at h2
+      exact h2 (Preorder.le_refl Outcome.P)
+    | L =>
+      -- Finally, if o(H + X) = L, then by (i) and (ii) we have o(G + X) > both P and N,
+      -- whence o(G + X) = L.
+      rw [ho] at h2
       rw [ho] at h3
       have h4 := h2 (Outcome.L_ge Outcome.P)
       have h5 := h3 (Outcome.L_ge Outcome.N)
       have h6 := Outcome.ge_PN h4 h5
       exact le_of_eq (Eq.symm h6)
-    · rw [ho] at h3
-      exact h3 (Preorder.le_refl Outcome.N)
-    · rw [ho] at h2
-      exact h2 (Preorder.le_refl Outcome.P)
-    · exact Outcome.ge_R (MisereOutcome (g + x))
 
 noncomputable def Adjoint (g : IGame) : IGame :=
   have _ := Classical.propDecidable (g = 0)
@@ -108,3 +161,48 @@ decreasing_by igame_wf
 theorem Adjont_zero_def : Adjoint 0 = {{⋆}|{⋆}}ᴵ := by
   unfold Adjoint
   simp only [reduceIte]
+
+theorem leftEmtpy_rightEmtpy_zero {g : IGame} (hl : g.leftMoves = ∅) (hr : g.rightMoves = ∅) : g = 0 := by
+  rw [IGame.zero_def]
+  ext
+  · simp [hl]
+  · simp [hr]
+
+theorem proposition6_4 {g : IGame} : MisereOutcome (g + Adjoint g) = Outcome.P := by
+  unfold MisereOutcome
+  unfold PlayerOutcomesToGameOutcome
+  -- FIXME: Comments and code are backwards
+  have h1 : MisereOutcomeL (g + Adjoint g) = PlayerOutcome.R := by
+    unfold MisereOutcomeL
+    have h2 : ¬(MisereOutcomeL_isL (g + Adjoint g)) := by
+      unfold MisereOutcomeL_isL
+      simp
+      constructor
+      · -- Now G + Adjoint G cannot be a Right end, since the definition of
+        -- Adjoint G ensures that it has at least one Right option
+        intro h3
+        unfold Adjoint
+        simp [h3]
+        by_cases h5 : g = 0
+        · simp [h5]
+        · simp [h5]
+          intro h6
+          exact h5 (leftEmtpy_rightEmtpy_zero h3 h6)
+      · intro k h3
+        -- So it suffices to show that Left has a winning response to every move by Right.
+        -- There are two cases:
+        -- If Right moves to G^R + Adjoint G or G + Adjont (G^L), Left has a mirror image move on
+        -- the other component, which wins by induction on G.
+        apply Or.elim h3
+          <;> intro h4
+          <;> obtain ⟨gl, ⟨h4, h5⟩⟩ := h4
+          <;> rw [<-h5]
+        · -- Show that G^L + Adjoint G is in R if Right goes first
+          sorry
+        · -- Show that G + (Adjoint G)^L is in R if Right goes first
+          sorry
+    simp [h2]
+  have h2 : MisereOutcomeR (g + Adjoint g) = PlayerOutcome.L := by
+    -- TODO: By symmetry
+    sorry
+  simp [h1, h2]
